ed29ded602d6622163650fd236b5c7c5c4fbf27a
pradeepk556 <115931372+pradeepk556@users.noreply.github.com>
2026-07-15 12:24:31 +0530

    Time: 1 ms (83.11%), Space: 42.6 MB (39.85%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -5,15 +5,8 @@ class Solution {
         return gcd(b, a % b);
     }
     public int gcdOfOddEvenSums(int n) {
-        int sumOdd = 0;
-        int sumEven = 0;
-        int num = 2;
-        for(int i = 0; i < n; i++) {
-            sumOdd += (num - 1);
-            sumEven += num;
-            num += 2;
-        }
-
+        int sumOdd = n * n;
+        int sumEven = sumOdd + n;
         return gcd(sumOdd, sumEven);
     }
 }
